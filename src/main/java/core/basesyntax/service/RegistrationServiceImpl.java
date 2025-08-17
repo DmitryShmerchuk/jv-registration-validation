58d400ca8cf0eb5b96df660b7fd396708b225a8e
Dmitry Shmerchuk <Dmitryvin1992@gmail.com>
2025-08-17 17:13:00 +0300

    Ready-3

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -9,6 +9,7 @@ public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_LOGIN_LENGTH = 6;
     private static final int MIN_PASSWORD_LENGTH = 6;
     private static final int MIN_AGE = 18;
+
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -16,19 +17,35 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user == null) {
             throw new RegistrationException("User can't be null");
         }
-        if (user.getLogin() == null || user.getLogin().length() < MIN_LOGIN_LENGTH) {
-            throw new RegistrationException("Login must be at least 6 characters");
+
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Login can't be null");
+        }
+        if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
+            throw new RegistrationException("Login must be at least "
+                    + MIN_LOGIN_LENGTH + " characters");
         }
         if (storageDao.get(user.getLogin()) != null) {
             throw new RegistrationException("User with login '" + user.getLogin()
                     + "' already exists");
         }
-        if (user.getPassword() == null || user.getPassword().length() < MIN_PASSWORD_LENGTH) {
-            throw new RegistrationException("Password must be at least 6 characters");
+
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Password can't be null");
         }
-        if (user.getAge() == null || user.getAge() < MIN_AGE) {
-            throw new RegistrationException("User must be at least 18 years old");
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RegistrationException("Password must be at least "
+                    + MIN_PASSWORD_LENGTH + " characters");
         }
+
+        if (user.getAge() == null) {
+            throw new RegistrationException("Age can't be null");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("User must be at least "
+                    + MIN_AGE + " years old");
+        }
+
         return storageDao.add(user);
     }
 }
